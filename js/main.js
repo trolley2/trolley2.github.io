1d8d14b89c97e6b6cf3a6084b5e96b2de3cd5702
trolley2 <rounitmishra@yahoo.in>
2017-02-21 21:37:44 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(window).load(function() {
 
 // PRELOADER
 $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(600); // set duration in brackets    
 });
 
 $(document).ready(function(){
